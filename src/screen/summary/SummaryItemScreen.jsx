c911c3ba556bc1d84e0576687def6f2f0eea5e1f
Dome <dome.erazo2606@gmail.com>
2023-07-19 22:21:36 -0500

    post-it

--- src/screen/summary/SummaryItemScreen.jsx
+++ src/screen/summary/SummaryItemScreen.jsx
@@ -5,6 +5,7 @@ import NavigatorPath from '../../components/NavigatorPath'
 import { getSummary } from '../../services/SummaryService'
 import { AuthContext } from '../../context/AuthContext'
 import { StatusBar } from 'expo-status-bar'
+import { FontAwesome } from '@expo/vector-icons'
 
 const SummaryItemScreen = ({ route }) => {
   const { summaryId } = route.params
@@ -30,12 +31,83 @@ const SummaryItemScreen = ({ route }) => {
     <View style={styles.container}>
       <NavigatorPath route={route} />
       <StatusBar style='dark' backgroundColor='white' />
+      <Text style={styles.title}>{titulo}</Text>
       <ScrollView contentContainerStyle={styles.contentContainer}>
-        <View style={styles.main}>
-          <Text style={styles.title}>{titulo}</Text>
-          <Text style={styles.subtitle}>Resumen</Text>
-          <Text style={styles.resumen}>{resumen}</Text>
+      <View style={styles.centeredView}>
+     
+        <View style={styles.postResumen}>
+            <Text style={[styles.resumen, { textAlign: 'justify' }]}>{resumen}</Text>
+          
+       
         </View>
+          <Text style={[styles.title, {fontSize:16}]}>Post-it o Algo así</Text>
+
+            {/* Palabras */}
+            <View style={[styles.post, {backgroundColor:COLORS.Pastel_Mint_Green}]}>
+          <View style={styles.firstColumn}>
+            <FontAwesome name='file-text-o' size={40} color={COLORS.Pastel_Mint_Green_1} > </FontAwesome>
+          </View>
+          <View style={styles.secondColumn}>
+            <Text style={[styles.subtitleP,{color:COLORS.Pastel_Mint_Green_1}]}>Palabras Clave</Text>
+            <View style={styles.row}>
+              <Text style={styles.texto}>Aquí encontrarás las fechas destacadas encontradas en el audio</Text>
+            </View>
+          </View>
+        </View>
+           {/* TEMAS */}
+           <View style={[styles.post, {backgroundColor: COLORS.GRAY}]}>
+          <View style={styles.firstColumn}>
+            <FontAwesome name='file-text-o' size={40} color={COLORS.GRAY_SOFT} > </FontAwesome>
+          </View>
+          <View style={styles.secondColumn}>
+            <Text style={[styles.subtitleP,{color:COLORS.GRAY_SOFT}]}>Temas Destacados</Text>
+            <View style={styles.row}>
+              <Text style={styles.texto}>Aquí encontrarás las fechas destacadas encontradas en el audio</Text>
+            </View>
+          </View>
+        </View>
+        {/* FECHAS  */}
+      
+        <View style={styles.post}>
+          <View style={styles.firstColumn}>
+            <FontAwesome name='calendar-o' size={40} color={COLORS.Pastel_Orange_1} > </FontAwesome>
+          </View>
+          <View style={styles.secondColumn}>
+            <Text style={[styles.subtitleP,{color:COLORS.Pastel_Orange_1}]}>Fechas importantes</Text>
+            <View style={styles.row}>
+              <Text style={styles.texto}>Aquí encontrarás las fechas destacadas encontradas en el audio</Text>
+            </View>
+          </View>
+        </View>
+     
+        {/* Preguntas */}
+        <View style={[styles.post, {backgroundColor:COLORS.Pastel_Pink}]}>
+          <View style={styles.firstColumn}>
+            <FontAwesome name='file-text-o' size={40} color={COLORS.Pastel_Pink_1} > </FontAwesome>
+          </View>
+          <View style={styles.secondColumn}>
+            <Text style={[styles.subtitleP,{color:COLORS.Pastel_Pink_1}]}>Preguntas</Text>
+            <View style={styles.row}>
+              <Text style={styles.texto}>Aquí encontrarás las fechas destacadas encontradas en el audio</Text>
+            </View>
+          </View>
+        </View>
+        
+         {/* Tareas */}
+         <View style={[styles.post, {backgroundColor:COLORS.Pastel_Blue}]}>
+          <View style={styles.firstColumn}>
+            <FontAwesome name='file-text-o' size={40} color={COLORS.Pastel_Blue_1} > </FontAwesome>
+          </View>
+          <View style={styles.secondColumn}>
+            <Text style={[styles.subtitleP,{color:COLORS.Pastel_Blue_1}]}>Tareas</Text>
+            <View style={styles.row}>
+              <Text style={styles.texto}>Aquí encontrarás las fechas destacadas encontradas en el audio</Text>
+            </View>
+          </View>
+        </View>
+
+
+      </View>
       </ScrollView>
     </View>
   )
@@ -49,29 +121,60 @@ const styles = StyleSheet.create({
     backgroundColor: COLORS.WHITE,
     width: '100%'
   },
-  contentContainer: {
+  centeredView: {
+    flex: 1,
     alignItems: 'center',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    marginTop: 20,
-    marginHorizontal: 25
+    marginTop: "4%",
+    
+    marginBottom: "50%",
   },
-  main: {
+ 
 
+  contentContainer: {
+    marginTop: 7,
+    marginHorizontal: 25,
   },
+
   title: {
     fontSize: 25,
     fontWeight: 'bold',
-    textAlign: 'center'
-  },
-  subtitle: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginTop: 20
+    textAlign: 'center',
+    marginTop: "8%",
+    color: COLORS.GRAY_SOFT
   },
   resumen: {
-    marginTop: 10,
-    fontSize: 16
-    // textAlign: 'justify'
-  }
+    fontSize: 16,
+    marginBottom: 10
+
+  },
+  post: {
+    flexDirection: 'row',
+    marginTop: "5%",
+    backgroundColor: COLORS.Pastel_Orange,
+    padding: 5,
+    borderRadius: 10
+  },
+  subtitleP: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    
+  },
+  texto: {
+    marginBottom: "2%",
+    color: COLORS.GRAY_EXTRA_SOFT
+  },
+  firstColumn: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  secondColumn: {
+    flex: 3,
+  },
+  row: {
+    flexDirection: 'row',
+    marginTop: 8,
+  },
+
+
 })
