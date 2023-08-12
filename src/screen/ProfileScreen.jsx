b75ee118b9fa85760b7b66e32a57cadb94189a75
alpalasaul <alpalasaul@gmail.com>
2023-08-12 18:27:44 -0500

    Refactor code

--- src/screen/ProfileScreen.jsx
+++ src/screen/ProfileScreen.jsx
@@ -9,6 +9,37 @@ import { useNavigation } from '@react-navigation/native'
 const { width } = Dimensions.get('window')
 const statusBarHeight = Constants.statusBarHeight
 
+const CustomIcon = ({ name, size = 20, color, type }) => {
+  const colors = color || COLORS.GRAY_EXTRA_SOFT
+  const filled = type === 'filled' ? { borderWidth: 0, backgroundColor: COLORS.GRAY, padding: 10 } : null
+  return (
+    <View style={[styles.optionIcon, { borderColor: colors }, filled]}>
+      <Ionicons name={name} size={size} color={colors} />
+    </View>
+  )
+}
+
+const OptionRow = ({ icon, onPress, typeIcon, colorIcon, children, rightIcon, disableIcon = false, style, ...restProps }) => {
+  const customRightIcon = rightIcon || <Ionicons name='chevron-forward-outline' size={20} color={COLORS.GRAY_EXTRA_SOFT} />
+  return (
+    <TouchableOpacity
+      style={{
+        ...styles.containerOption,
+        ...style
+      }}
+      onPress={onPress}
+      disabled={disableIcon}
+      {...restProps}
+    >
+      <CustomIcon name={icon} type={typeIcon} color={colorIcon} />
+      <Text style={styles.optionText}>{children}</Text>
+      <View style={[styles.optionRowIcon]}>
+        {customRightIcon}
+      </View>
+    </TouchableOpacity>
+  )
+}
+
 const ProfileScreen = () => {
   const [isDarkModeEnabled, setIsDarkModeEnabled] = useState(true)
   const [fontSize, setFontSize] = useState(16)
@@ -35,105 +66,56 @@ const ProfileScreen = () => {
         </View>
 
         {/* ACCOUNT */}
-        <Text style={styles.tit}>Cuenta</Text>
-        <TouchableOpacity style={styles.izq} onPress={() => navigation.navigate(personalInforScreenName)}>
-          <View style={styles.account}>
-            <View style={styles.square}>
-              <Ionicons name='person-outline' size={20} color={COLORS.ORANGE} />
-            </View>
-            <Text style={styles.texto}>Información Personal</Text>
-            <View style={styles.flecha}>
-              <Ionicons name='chevron-forward-outline' size={20} color={COLORS.GRAY_EXTRA_SOFT} />
-            </View>
-          </View>
-        </TouchableOpacity>
-        <View style={styles.line} />
+        <Text style={styles.titulo}>Cuenta</Text>
+
+        <OptionRow
+          icon='person-outline'
+          colorIcon={COLORS.ORANGE}
+          onPress={() => navigation.navigate(personalInforScreenName)}
+        >
+          Información Personal
+        </OptionRow>
+
+        <View style={styles.separator} />
 
         {/* APPEARANCE */}
-        <Text style={styles.tit}>Apariencia</Text>
-        <View style={styles.account}>
-          <View style={styles.squareA}>
-            <Ionicons name='moon-outline' size={20} color={COLORS.GRAY_EXTRA_SOFT} />
-          </View>
-          <Text style={styles.texto}>Modo Noche</Text>
-          <View style={styles.flecha}>
-            <Switch value={isDarkModeEnabled} onValueChange={toggleDarkMode} />
-          </View>
-        </View>
-        <View style={styles.account}>
-          <View style={styles.squareA}>
-            <Ionicons name='text' size={20} color={COLORS.GRAY_EXTRA_SOFT} />
-          </View>
-          <Text style={styles.texto}>Tamaño de letra</Text>
-          <View style={styles.flecha1}>
-            <TouchableOpacity onPress={decreaseFontSize}>
-              <Ionicons name='remove' size={15} color='black' />
-            </TouchableOpacity>
-            <Text style={styles.fontSizeText}>{fontSize}</Text>
-            <TouchableOpacity onPress={increaseFontSize}>
-              <Ionicons name='add' size={15} color='black' />
-            </TouchableOpacity>
-          </View>
-        </View>
-        <View style={styles.line} />
+        <Text style={styles.titulo}>Apariencia</Text>
 
-        <Text style={styles.tit}>Otras Configuraciones</Text>
-        <TouchableOpacity style={styles.izq}>
-          <View style={styles.account}>
-            <View style={styles.squareB}>
-              <Ionicons name='lock-closed-outline' size={20} color={COLORS.GRAY_EXTRA_SOFT} />
+        <OptionRow
+          icon='moon-outline'
+          rightIcon={<Switch value={isDarkModeEnabled} onValueChange={toggleDarkMode} />}
+          disableIcon
+        >
+          Modo noche
+        </OptionRow>
+        <OptionRow
+          icon='text'
+          rightIcon={
+            <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+              <TouchableOpacity onPress={decreaseFontSize} style={{ padding: 5 }}>
+                <Ionicons name='remove' size={15} color='black' />
+              </TouchableOpacity>
+              <Text style={styles.fontSizeText}>{fontSize}</Text>
+              <TouchableOpacity onPress={increaseFontSize} style={{ padding: 5 }}>
+                <Ionicons name='add' size={15} color='black' />
+              </TouchableOpacity>
             </View>
-            <Text style={styles.texto}>Seguridad</Text>
-            <View style={styles.flecha}>
-              <Ionicons name='chevron-forward-outline' size={20} color={COLORS.GRAY_EXTRA_SOFT} />
-            </View>
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.izq}>
-          <View style={styles.account}>
-            <View style={styles.squareB}>
-              <Ionicons name='shield-checkmark-outline' size={20} color={COLORS.GRAY_EXTRA_SOFT} />
-            </View>
-            <Text style={styles.texto}>Privacidad</Text>
-            <Ionicons name='alert-circle-outline' size={20} color={COLORS.ORANGE} style={styles.prueba} />
-            <View style={styles.flecha}>
-              <Ionicons name='chevron-forward-outline' size={20} color={COLORS.GRAY_EXTRA_SOFT} />
-            </View>
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.izq}>
-          <View style={styles.account}>
-            <View style={styles.squareB}>
-              <Ionicons name='notifications-outline' size={20} color={COLORS.GRAY_EXTRA_SOFT} />
-            </View>
-            <Text style={styles.texto}>Notificaciones</Text>
-            <View style={styles.flecha}>
-              <Ionicons name='chevron-forward-outline' size={20} color={COLORS.GRAY_EXTRA_SOFT} />
-            </View>
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.izq}>
-          <View style={styles.account}>
-            <View style={styles.squareB}>
-              <Ionicons name='help-circle-outline' size={20} color={COLORS.GRAY_EXTRA_SOFT} />
-            </View>
-            <Text style={styles.texto}>Ayuda y Soporte</Text>
-            <View style={styles.flecha}>
-              <Ionicons name='chevron-forward-outline' size={20} color={COLORS.GRAY_EXTRA_SOFT} />
-            </View>
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.izq}>
-          <View style={styles.account}>
-            <View style={styles.squareB}>
-              <Ionicons name='chatbox-ellipses-outline' size={20} color={COLORS.GRAY_EXTRA_SOFT} />
-            </View>
-            <Text style={styles.texto}>Realimentación</Text>
-            <View style={styles.flecha}>
-              <Ionicons name='chevron-forward-outline' size={20} color={COLORS.GRAY_EXTRA_SOFT} />
-            </View>
-          </View>
-        </TouchableOpacity>
+          }
+          disableIcon
+        >
+          Tamaño de letra
+        </OptionRow>
+
+        <View style={styles.separator} />
+
+        {/* OTHERS */}
+        <Text style={styles.titulo}>Otras Configuraciones</Text>
+
+        <OptionRow icon='lock-closed-outline' typeIcon='filled'>Seguridad</OptionRow>
+        <OptionRow icon='shield-checkmark-outline' typeIcon='filled'>Privacidad</OptionRow>
+        <OptionRow icon='notifications-outline' typeIcon='filled'>Notificaciones</OptionRow>
+        <OptionRow icon='help-circle-outline' typeIcon='filled'>Ayuda y Soporte</OptionRow>
+        <OptionRow icon='chatbox-ellipses-outline' typeIcon='filled'>Realimentación</OptionRow>
 
       </View>
     </View>
@@ -147,6 +129,27 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: COLORS.WHITE,
     paddingTop: statusBarHeight,
+    alignItems: 'center'
+  },
+  containerOption: {
+    marginTop: 15,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  optionIcon: {
+    borderWidth: 2,
+    borderColor: COLORS.ORANGE,
+    borderRadius: 5,
+    padding: 8
+  },
+  optionText: {
+    flex: 1,
+    marginLeft: 20
+  },
+  optionRowIcon: {
+    paddingHorizontal: 20,
+    flexDirection: 'row',
     alignItems: 'center'
   },
   contentContainer: {
@@ -163,46 +166,14 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     marginTop: 80
   },
-  account: {
-    flexDirection: 'row',
-    alignSelf: 'flex-start'
-    // marginLeft: '7%'
-  },
-  tit: {
+  titulo: {
     fontWeight: '500',
     fontSize: 16,
     color: COLORS.GRAY_EXTRA_SOFT,
     marginTop: '4%',
     alignSelf: 'flex-start'
   },
-  square: {
-    marginTop: '4%',
-    width: 40,
-    height: 40,
-    borderWidth: 2,
-    borderColor: COLORS.ORANGE,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 4
-  },
-  texto: {
-    padding: 20,
-    color: COLORS.GRAY_SOFT
-  },
-
-  flecha1: {
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-    position: 'absolute',
-    zIndex: 2,
-    paddingTop: 20,
-    marginLeft: '80%'
-  },
-  flecha2: {
-    flexDirection: 'row'
-  },
-  line: {
+  separator: {
     width: width * 0.9,
     height: 2,
     backgroundColor: COLORS.GRAY,
@@ -211,55 +182,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center'
   },
-  squareA: {
-    marginTop: '4%',
-    width: 40,
-    height: 40,
-    borderWidth: 2,
-    borderColor: COLORS.GRAY_EXTRA_SOFT,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 4
-  },
-  prueba: {
-    position: 'absolute',
-    paddingTop: 20,
-    marginLeft: '80%',
-    zIndex: 2
-  },
-  squareB: {
-    marginTop: '4%',
-    width: 40,
-    height: 40,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 4,
-    backgroundColor: COLORS.GRAY
-  },
-  izq: {
-    alignSelf: 'flex-start'
-  },
-  option: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    marginBottom: 8
-  },
-  optionLabel: {
-    fontSize: 16
-  },
-  fontSizeContainer: {
-    flexDirection: 'row',
-    alignItems: 'center'
-  },
   fontSizeText: {
-    fontSize: 16,
     marginHorizontal: 8
-  },
-  flecha: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'flex-end',
-    paddingRight: 20
   }
 })
